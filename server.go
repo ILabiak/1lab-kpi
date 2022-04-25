b80baeb21e85126a04e979c419a286137c41ecce
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-04-25 16:38:02 +0300

    initial server code

--- server.go
+++ server.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	//  "fmt"
-	//  "html"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
-func main() {
-
-	log.Fatal(http.ListenAndServe(":8795", nil))
-
+func getTime(w http.ResponseWriter, req *http.Request) {
+	currentTime := time.Now()
+	timeWithTimestamp := currentTime.Format(time.RFC3339)
+	data := map[string]string{
+		"time": timeWithTimestamp,
+	}
+	b, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if req.Method == "GET" {
+		fmt.Fprintf(w, string(b))
+	} else {
+		fmt.Fprintf(w, "Sorry, only GET request is supported.")
+	}
 }
-//comment1
+
+func main() {
+	http.HandleFunc("/time", getTime)
+
+	fmt.Printf("Starting server on ")
+	if err := http.ListenAndServe(":8795", nil); err != nil {
+		log.Fatal(err)
+	}
+}
